69760826242d9017df0058098651c5554f541ba5
StephanRaab <sarcartist@gmail.com>
2017-03-09 18:53:16 -0500

    Lesson 3 Problem set Completed

--- lesson3.py
+++ lesson3.py
@@ -112,12 +112,30 @@
 # print("65 = ", chr(65))
 
 # STRING PROBLEM
-# Enter a string to hide in uppercase : HIDE
+# Enter a string to hide: test string
+# string to upper()
 # convert to unicode numbers --> secret message : "35647890"
 # Original message : HIDE
 
+# MY HALF ANSWER
 message = input("Enter a string to hide: ")
 hiddenMessage = message.upper()
 for i in range(0, len(hiddenMessage)):
-    print(ord(hiddenMessage[i]), end='')
+    new_string = str(ord(hiddenMessage[i]))
+    print(new_string, end='')
 
+
+# DEREK'S ANSWER
+norm_string = input("Enter a string to hide: ")
+
+secret_string = ""
+
+for char in norm_string:
+    secret_string += str(ord(char) -23)
+print("Secret Message: ", secret_string)
+
+norm_string = ""
+for i in range(0, len(secret_string)-1, 2):
+    char_code = secret_string[i] + secret_string[i+1]
+    norm_string += chr(int(char_code) + 23)
+print("Original Message: ", norm_string)
